3912d76437a6998e67801e6dc55bed93b2a6cb42
René Pagel <66637830+RePag-de@users.noreply.github.com>
2020-08-29 10:10:37 +0200

    Package eingeführt Libraries entfernt Footer aktualisiert

--- Libraries/User.h
+++
@@ -1,8 +0,0 @@
-// ab Dateiversion 1.0.1.101
-#ifndef UserH
-#define UserH
-//---------------------------------------------------------------------------
-__declspec(dllimport) void __vectorcall CODEC(_In_ HWND hWnd);
-__declspec(dllimport) void __vectorcall CPUInstructions(_In_ HWND hWnd);
-//---------------------------------------------------------------------------
-#endif
